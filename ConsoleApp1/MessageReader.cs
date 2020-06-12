32f7d820701eb6dc0b15d72de7b1305909ba1357
Carlos Pedro <carlospedr@gmail.com>
2020-06-12 10:43:45 +0200

    Move types to apropriate files

--- ConsoleApp1/MessageReader.cs
+++ ConsoleApp1/MessageReader.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
@@ -20,31 +19,6 @@ namespace ConsoleApp1
                 }
                 currentMessage.Add(l);
             }
-        }
-    }
-
-    public interface IPortExtractor
-    {
-        int Port(string record);
-    }
-
-    public class PortExtractor : IPortExtractor
-    {
-        private readonly Regex _portRegex = new Regex(@"audio (\d+) RTP\/AVP");
-        public int Port(string record)
-        {
-
-            var match = _portRegex.Match(record);
-            if (match.Groups.Count < 2)
-            {
-                throw new InvalidPortData();
-            }
-
-            if(int.TryParse(match.Groups[1].Value, out int port))
-            {
-                return port;
-            }
-            throw new InvalidPortData();
         }
     }
 }
---
+++ ConsoleApp1/PortExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    public class PortExtractor : IPortExtractor
+    {
+        private readonly Regex _portRegex = new Regex(@"audio (\d+) RTP\/AVP");
+        public int Port(string record)
+        {
+
+            var match = _portRegex.Match(record);
+            if (match.Groups.Count < 2)
+            {
+                throw new InvalidPortData();
+            }
+
+            if(int.TryParse(match.Groups[1].Value, out int port))
+            {
+                return port;
+            }
+            throw new InvalidPortData();
+        }
+    }
+}
