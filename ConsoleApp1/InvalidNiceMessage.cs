ef78aaf0d6563b4b99d77da13e82c4932fff3be4
Carlos Pedro <carlospedr@gmail.com>
2020-06-12 11:32:39 +0200

    Add handling of invalid messages

---
+++ ConsoleApp1/InvalidNiceMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class InvalidNiceMessage : NiceMessage
+    {
+        private List<string> _payload;
+
+        public InvalidNiceMessage(List<string> payload) : base(string.Empty, 0, null)
+        {
+            _payload = payload;
+        }
+
+        public override string ToString()
+        {
+            return $"Invalid Message with payload: {string.Join('n', _payload)}";
+        }
+    }
+}
--- ConsoleApp1/MessageParser.cs
+++ ConsoleApp1/MessageParser.cs
@@ -21,49 +21,54 @@ namespace ConsoleApp1
             string ip = null;
             int? port = null;
             List<string> codecs = new List<string>();
-
-            foreach (var line in messageLines)
+            try
             {
-                var keyValue = line.Split('=');
-
-                if (keyValue.Length != 2)
-                    throw new InvalidMessageFormat();
-
-                switch (keyValue[0])
+                foreach (var line in messageLines)
                 {
-                    case "c":
-                        ip = _ipExtractor.Ip(keyValue[1]);
-                        break;
-                    case "m":
-                        port = _portExtractor.Port(keyValue[1]);
-                        break;
-                    case "a":
-                        var codec = _codecExtractor.Codec(keyValue[1]);
-                        if (!string.IsNullOrWhiteSpace(codec))
-                        {
-                            codecs.Add(codec);
-                        }
-                        break;
-                    default:
-                        break;
+                    var keyValue = line.Split('=');
+
+                    if (keyValue.Length != 2)
+                        throw new InvalidMessageFormat();
+
+                    switch (keyValue[0])
+                    {
+                        case "c":
+                            ip = _ipExtractor.Ip(keyValue[1]);
+                            break;
+                        case "m":
+                            port = _portExtractor.Port(keyValue[1]);
+                            break;
+                        case "a":
+                            var codec = _codecExtractor.Codec(keyValue[1]);
+                            if (!string.IsNullOrWhiteSpace(codec))
+                            {
+                                codecs.Add(codec);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+
                 }
 
-            }
+                if (!port.HasValue)
+                {
+                    throw new InvalidMessageFormat();
+                }
+                if (string.IsNullOrWhiteSpace(ip))
+                {
+                    throw new InvalidMessageFormat();
+                }
 
-            if (!port.HasValue)
-            {
-                throw new InvalidMessageFormat();
+                if (codecs.Count == 0)
+                {
+                    throw new InvalidMessageFormat();
+                }
             }
-            if (string.IsNullOrWhiteSpace(ip))
+            catch(InvalidMessageFormat)
             {
-                throw new InvalidMessageFormat();
+                return new InvalidNiceMessage(messageLines);
             }
-
-            if (codecs.Count == 0)
-            {
-                throw new InvalidMessageFormat();
-            }
-
             return new NiceMessage(ip, port.Value, codecs);
         }
     }
