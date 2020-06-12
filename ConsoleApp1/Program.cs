af78233ca362dead23b4811094147666542f8b53
Carlos Pedro <carlospedr@gmail.com>
2020-06-12 09:39:24 +0200

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines(@"C:\Users\Carlos\Desktop\sdp_input.txt");
+            foreach(var l in lines)
+            {
+                Console.WriteLine(l);
+            }
+        }
+
+        private IEnumerable<List<string>> ReadMessageLines(string filePath)
+        {
+            var lines = File.ReadLines(@"C:\Users\Carlos\Desktop\sdp_input.txt");
+            var currentMessage = new List<string>();
+            foreach (var l in lines)
+            {
+                if(string.IsNullOrWhiteSpace( l))
+                {
+                    yield return currentMessage;
+                    currentMessage = new List<string>();
+                }
+                currentMessage.Add(l);
+                
+            }
+        }
+    }
+}
